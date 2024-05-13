09e9d024b63a92ed8278586786b871fd393939c5
BINET aurélie <au.binet21@gmail.com>
2024-05-13 10:32:42 +0200

    ECF

---
+++ Habitat.php
@@ -0,0 +1,23 @@
+<img id="myImage" src="../assets/Habitats/La_jungle.png" alt="images" width="300" height="300">
+
+    <div id="text" style="display: block;">
+
+
+<p>Description de l'habitat de la la jungle:Forme de savane touffue (hautes herbes, broussailles, arbres) où vivent les grands fauves.
+        Des grands animaux existent et cohabitent entre eux.
+       
+    <img src=assets/image/LION.png>
+    <p>Un animal est caractérsé par son prénom:simba (le LION)
+        Race:Ces lions constituent l'une des plus grandes espèces de lions connues</p>
+    <img src=assets/image/Singe.png>
+    <p>un animal est caractérisé pour son prénom :Shopi
+    Race: Ces petits singes font le bonheur des touristes.On les appelle Les Ouistitis ou Callitriches.</p>
+    <img src=assets/image/serpent.png alt="images" width="150" height="150">
+    <p>Un animal est caractérisé par ces couleurs reconnaissable comme le serpent Mosaique Albinos
+    Race :Mosaique Albinos caractérisée par ces couleurs orange et blanc </p>
+    <img src=assets/image/koala.png>
+   <p> Ce petit animal est un petit paresseux ,il fait partie de la famille des marsupiaux 
+    Race :Marsupiaux</p>
+    <img src= assets/image/elephant>
+    <p>Caractérisé par son poids et sa hauteur,cet animal est reconnaissable parmi les animaux:l'éléphant
+    Race: les pachydermes </p>
