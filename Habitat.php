e7f1ce680eb6101483d57a489f7f75f86f62e012
BINET aurélie <au.binet21@gmail.com>
2024-05-13 13:55:09 +0200

    IMAGES services et animaux

--- Habitat.php
+++ Habitat.php
@@ -1,23 +1,85 @@
-<img id="myImage" src="../assets/Habitats/La_jungle.png" alt="images" width="300" height="300">
+<img id="myImage" src="assets/Habitats/La_jungle.png" alt="images" width="300" height="300">
 
-    <div id="text" style="display: block;">
+<div id="text" style="display: block;">
 
 
-<p>Description de l'habitat de la la jungle:Forme de savane touffue (hautes herbes, broussailles, arbres) où vivent les grands fauves.
-        Des grands animaux existent et cohabitent entre eux.
-       
+    <p>Description de l'habitat de la la jungle:Forme de savane touffue (hautes herbes, broussailles, arbres) où vivent les grands fauves.
+        Des grands animaux existent et cohabitent entre eux.</p>
+
     <img src=assets/image/LION.png>
     <p>Un animal est caractérsé par son prénom:simba (le LION)
         Race:Ces lions constituent l'une des plus grandes espèces de lions connues</p>
     <img src=assets/image/Singe.png>
     <p>un animal est caractérisé pour son prénom :Shopi
-    Race: Ces petits singes font le bonheur des touristes.On les appelle Les Ouistitis ou Callitriches.</p>
+        Race: Ces petits singes font le bonheur des touristes.On les appelle Les Ouistitis ou Callitriches.</p>
     <img src=assets/image/serpent.png alt="images" width="150" height="150">
     <p>Un animal est caractérisé par ces couleurs reconnaissable comme le serpent Mosaique Albinos
-    Race :Mosaique Albinos caractérisée par ces couleurs orange et blanc </p>
+        Race :Mosaique Albinos caractérisée par ces couleurs orange et blanc </p>
     <img src=assets/image/koala.png>
-   <p> Ce petit animal est un petit paresseux ,il fait partie de la famille des marsupiaux 
-    Race :Marsupiaux</p>
-    <img src= assets/image/elephant>
+    <p> Ce petit animal est un petit paresseux ,il fait partie de la famille des marsupiaux
+        Race :Marsupiaux</p>
+    <img src=assets/image/elephant.png>
     <p>Caractérisé par son poids et sa hauteur,cet animal est reconnaissable parmi les animaux:l'éléphant
-    Race: les pachydermes </p>
+        Race: les pachydermes </p>
+
+    <h4>Le marais</h4>
+        <img id="myImage" src="assets/Habitats/images.png" alt="images" width="300" height="300">
+<div id="text" style="display: block;">
+        <p>Description du marais :Nappe d'eau stagnante recouvrant un terrain partiellement envahi par la végétation.
+            De grands animaux y cohabitent :
+            Amphibiens. Pélodyte ponctué Triton marbré ...
+            Mammifères. Grand Rhinolophe. Loutre d'Europe. ...
+            Espèces envahissantes. Ragondin. Écrevisse américaine. ...
+            Insectes. Cuivré des marais. Rosalie des Alpes. ...
+            Oiseaux. Héron garde-boeufs. Gorgebleue à miroir. ...
+            Poissons. Gardon. Grande alose et Alose feinte. ...
+            Reptiles. Couleuvre à collier.
+            <img src=assets/Habitats/ragondin.png alt="images" width="150" height="150">
+            <p>un animal est caractérisé par
+            son prénom:Ragondin
+            sa race:Myocastor coypus, plus simplement, le ragondin, aussi appelé « castor des marais », ou « loutre d'Amérique »,
+            appartient à la famille des Myocastoridés, des mammifères rongeurs </p>
+            <img src =assets/image/Herongarde_boeuf.png>
+           <p>un animal est caractérisé par
+            Son prénom : Herongarde_boeuf
+            Race:Oiseaux 
+            <img src =assets/image/loutre_deurope.png>
+           <p>un animal est caractérisé par
+            Son prénom : loutre d europe
+            Race:Mammifère se trouvant hors ou sous l'eau.</p>
+            <img src = assets/image/rosalie_des_alpes.png>
+           <p>Son prénom:rosalie_des_alpes
+            Race:insecte au bord du marais</p>
+
+
+        <img id="myImage" src="assets/Habitats/Savane.png" alt="images" width="300" height="300">
+
+        <div id="text" style="display:block">
+    <h4>la savane</h4>
+            <p>Description de la savane:Vaste prairie des régions tropicales, pauvre en arbres et en fleurs
+                Les animaux de la savane:
+                Antilope noire. Ces puissantes antilopes peuvent atteindre une hauteur de 140 cm
+                et se vantent de posséder de majestueuses cornes cerclées. ...
+                Babouin.
+                Chacal.
+                Fennec.
+                Girafe.
+                Lion.
+                Mangouste
+                Oryctérope du Cap.
+                Prenons l'exemple de la girafle :elle est caractérisé par
+                son prénom : Giraffon
+                sa race:Girafe du niger (mais beaucoup d'espèces existent)
+                son habitat : la Savane </p>
+            <img src=assets/Habitats/GIRAFE1.png>
+            <p>un animal est caractérisé par
+            Son prénom : la girafe 
+            Race:Reine géante et majestueuse</p>
+            <img src=assets/image/LION.png>
+            Son prénom:le lion
+            Race: Lion de la savane 
+        </div>
+    </div>
+
+
+
---
+++ services.php
@@ -0,0 +1,12 @@
+<h1>VUE GLOALE DE TOUS LES SERVICES </h1>
+
+
+<div class="card" style="width: 18rem;">
+<img src=assets/services/restauration.png>
+  <div class="card-body">
+    <h5 class="card-title">restauration</h5>
+    <p class="card-text">Une pause</p>
+    <a href="#" class="btn btn-primary"> <Allons-y> </a>
+  </div>
+</div>
+
