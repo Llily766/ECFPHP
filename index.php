96607af2f8f823468879e99e8b7d0b32cffd05e7
BINET aurélie <au.binet21@gmail.com>
2024-05-13 08:18:02 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arcadia</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <h1>ARCADIA</h1>
+      <h4>  Arcadia est située en France,près de la forêt de brocéliande ,en bretagne, depuis 1960.
+    Ils possèdent tout un panel d'animaux et essaye de reconstituer à l'identique leurs habitats :
+    savane,
+    marais,
+    jungle,
+    Les animaux sont inspectés chaque jour par un vétérinaire.
+
+    D un point de vue financier ,le zoo se porte bien ,cela fait plaisir à José,directeur de ce zoo.
+    A ce jour, José n'a pas de site web pour son zoo ,par conséquent il a mandaté Josette pour
+    trouver une entreprise ,qui va lui permettre de créer son site web.
+    Après l'obtention du diplome , j'ai été mandaté au sein de la société DEVSoft
+    pour réaliser leur site web.
+    Précision importante de José:“Je souhaite avoir une
+    application web dont les couleurs et le thème font penser à de l’écologie, le zoo est
+    entièrement indépendant au niveau des énergies et il faut que lorsqu’un utilisateur consulte
+    l’application, il ressente les valeurs de l’écologie dont nous sommes fiere”</h4>
+
+<h4> Les différents habitats sont la Savane;la Jungle;le Marais.
+    Les animaux sont :
+    les singes,
+    les éléphants,
+    les oiseaux,
+    les crocodiles,
+    les dauphins,
+    les otaries et bien plus encore</h4>
+
+    <img src=assets/image/LION.png>
+    <h4> Zoom sur les avis du zoo: Très beau parc;
+    Un immense zoo ,à découvrir;
+    Les animaux sont heureux ,on le resssent.</h4>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
